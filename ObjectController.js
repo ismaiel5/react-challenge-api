f102de1c57f3797e7b0d07aa67b286c538941dfd
ismaiel <ismaiel.altahan@gmail.com>
2021-06-27 13:29:01 +0800

    -added object controller where it will have the functions related

---
+++ ObjectController.js
@@ -0,0 +1,7 @@
+exports.getObjectsInfo = function (req, res) {
+  res.status(200).send("get objects info function");
+};
+
+exports.getFile = function (req, res) {
+  res.status(200).send("get file function");
+};
