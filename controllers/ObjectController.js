17998aa48fdd6a83f11e072306880de3872c98a2
ismaiel <ismaiel.altahan@gmail.com>
2021-06-27 22:50:45 +0800

    -update and added CORS to the getrandomobject function to get accept calls from the localhost:3000

--- controllers/ObjectController.js
+++ controllers/ObjectController.js
@@ -1,15 +1,18 @@
-var utilities = require('../utils/utilities');
-
-
+var utilities = require("../utils/utilities");
 
 
 exports.getRandomObjects = function (req, res) {
-  
-  var objectGenerated =  utilities.runGeneratingFuncRandomly(20);
-
-  res.status(200).send(objectGenerated);
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  try {
+    var objectGenerated = utilities.runGeneratingFuncRandomly(50);
+    res.status(200).send(objectGenerated);
+  } catch (error) {
+    console.error(error);
+    res.status(400).send("please try again..");
+  }
 };
 
 exports.getFile = function (req, res) {
-  res.status(200).send("get file function");
+
+  res.status(200).sendFile('get file function');
 };
