6c59fe1b4ec4de56b5ac907baf686fa9aa863ab0
ismaiel <ismaiel.altahan@gmail.com>
2021-06-27 13:32:50 +0800

    - update the index.js to use the routes and controller instead of having the functions in the index file.

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 var express = require("express");
+const routes = require("./Routes");
 app = express();
 PORT = 8080;
+
+app.use("/api/", routes);
 
 app.listen(PORT, (error) => {
   if (error) throw error;
   console.log(`I am new express api and I am alive on http://localhost:${PORT}`);
 });
-
-app.get("/objectsData", (req, res) => {
-  const object = { name: "ismaiel" };
-  res.status(200).send(object);
-});
-
-
-
-app.get("/file", (req, res) => {
-    const object = { name: "ismaiel" };
-    res.status(200).send(object);
-  });
-  
